be50a912910ab8ef3b6b74ab17c8d622aef72d8c
nathruts <91705274+Naa-Lopes@users.noreply.github.com>
2022-08-19 20:52:03 -0300

    Initial commit

---
+++ resources/views/listagem.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-vidth, initial scale=1">
+        <title>LISTA DE USUARIOS</title>
+</head>
+<body>
+    <h1>Lista de Usuários</h1>
+
+    <table>
+    <tr>
+        <td>Nome</td>
+        <td>Email</td>
+        <td>Data Nasc</td>
+        <td>Telefone</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Alfredo</td>
+        <td>Alfredo123@gmail.com</td>
+        <td>27/09/2003</td>
+        <td>987654325</td>
+    </tr>
+    <tr>
+        <td>Ralf</td>
+        <td>Ralf321@gmail.com</td>
+        <td>05/03/2001</td>
+        <td>943567892</td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-vidth, initial scale=1">
+        <title>HOME</title>
+</head>
+<body>
+    <h1>PAG INICIAL</h1>
+    <h3>http://127.0.0.1:8000/ (Caminha para a página Inicial)</h3>
+    <h3>http://127.0.0.1:8000/cadastro (Caminha para a página de cadastro)</h3>
+    <h3>http://127.0.0.1:8000/listagem (Caminha para a página de listagem)</h3>
+</body>
+</html>
